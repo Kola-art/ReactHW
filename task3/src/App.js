606277d15f2aaee3c36bb96eb063e0dfbb28a2e7
Artem Kolodka <73796530+Kola-art@users.noreply.github.com>
2021-03-16 18:02:38 +0200

    task3

---
+++ task3/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component  {
+    constructor(props){
+      super(props);
+      this.state = {
+        userName: 'гость',
+      }
+     
+    }
+ 
+    greetingText(){
+      return(
+        <p>Привет, { this.state.userName }  !  </p>
+      )
+    }
+    handleChange(event){
+      if(event.target.value == ""){
+        this.setState({userName : 'гость'});
+      }
+      else this.setState({userName : event.target.value});
+      return (  
+        this.greetingText()
+    )
+    
+    }
+  render(){
+    return (
+     
+          <div className="App">
+            {this.greetingText() }
+         
+            <input type="text" placeholder="Введите Ваше имя" onBlur ={(event) => this.handleChange(event)}></input>
+          </div>
+      
+    );
+  }
+}
+
+export default App;
